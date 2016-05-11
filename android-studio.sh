1d7e7077cce893b187b84bf32b0967196bcbcaf7
ashwini <geek.ashwini@gmail.com>
2016-05-12 04:31:33 +0530

    Android Studio

---
+++ android-studio.sh
@@ -0,0 +1,15 @@
+#This script will install Android Studio in Ubuntu and derivatives like Linux Mint etc.
+
+#Add the repository
+
+sudo add-apt-repository ppa:paolorotolo/android-studio
+
+#Update
+
+sudo apt-get update
+
+#Install Android Studio
+
+sudo apt-get install android-studio
+
+#Done!
