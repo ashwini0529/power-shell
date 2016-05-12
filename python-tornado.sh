14c9fa33ce37b6a8d11e191f6846c958d3e988bf
ashwini <geek.ashwini@gmail.com>
2016-05-12 06:02:42 +0530

    Tornado server

---
+++ python-tornado.sh
@@ -0,0 +1,18 @@
+#Some libraries that should be installed to use Tornado framework.
+
+
+#Install PIP if not installed
+
+sudo apt-get install python-pip
+
+#install tornado
+
+sudo pip install tornado
+
+#install pymongo
+
+sudo pip install pymongo
+
+#install motor
+
+sudo pip install motor
