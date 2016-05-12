6402b30c1fcb39eef3e12286b0327b5cb373be51
ashwini <geek.ashwini@gmail.com>
2016-05-12 06:04:18 +0530

    sudo access

--- lamp-stack-14-04.sh
+++ lamp-stack-14-04.sh
@@ -1,20 +1,18 @@
 # A shell script to install LAMP (Linux, Apache, MYSQL,PHP) on Ubuntu 14.04 LTS)
 
-#Root access
 
-sudo su
 
 #Update 
 
-apt-get update
+sudo apt-get update
 
 #Install apache
 
-apt-get install apache2
+sudo apt-get install apache2
 
 #Install MYSQL
 
-apt-get install mysql-server php5-mysql
+sudo apt-get install mysql-server php5-mysql
 
 #Setup mysql db
 
@@ -23,7 +21,7 @@ mysql_install_db
 
 #Install PHP
 
-apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
 
 
 :'
@@ -33,11 +31,11 @@ We want to move the PHP index file highlighted above to the first position after
     DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm
 </IfModule>
 '
-sed -i -- 's/DirectoryIndex index.html index.cgi index.pl index.php index.xhtml index.htm/DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/g' /etc/apache2/mods-enabled/dir.conf
+sudo sed -i -- 's/DirectoryIndex index.html index.cgi index.pl index.php index.xhtml index.htm/DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/g' /etc/apache2/mods-enabled/dir.conf
 
 
 #Restart apache Server 
 
-service apache2 restart
+sudo service apache2 restart
 
 #Finished installation of lamp stack
