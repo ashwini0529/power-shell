69b56fda05e693d990a8be2b15c1b0a0d88185b8
Ashwini Purohit <ashwinipurohit@yahoo.in>
2016-05-20 16:26:59 +0530

    Install Developer python before Tornado packages

--- python-tornado.sh
+++ python-tornado.sh
@@ -1,18 +1,23 @@
 #Some libraries that should be installed to use Tornado framework.
-
+#Don't put -y after any pip install packagename.
+#It does not support -y . It takes -y as a parameter.
 
 #Install PIP if not installed
 echo '************Installing pip************'
 sudo apt-get install python-pip -y
 
+#Install Developer Python before installing motor or pymongo
+
+sudo apt-get install python-dev
+
 #install tornado
 echo '************Installing Tornado************'
-sudo pip install tornado -y
+sudo pip install tornado 
 
 #install pymongo
 echo '************Installing PyMongo************'
-sudo pip install pymongo -y
+sudo pip install pymongo 
 
 #install motor
 echo '************Installing Motor************'
-sudo pip install motor -y
+sudo pip install motor
