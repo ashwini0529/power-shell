be2abb0b4e047ba52b3ce619024ca2c131408592
Mehul <blackdragon0909@gmail.com>
2016-10-27 03:37:14 +0530

    fixed the script

--- opencv.sh
+++ opencv.sh
@@ -8,46 +8,38 @@
 
 # __BEGIN_MAIN__
 
-if ! [ $(id -u) = 0 ]; then
-   echo "This installer must be run as root :/"
-   exit 1
-fi
-mkdir setup
-cd setup
-cores=$(grep -c ^processor /proc/cpuinfo)
-cores=$(($cores * 2))
-Sourcesystem=$(uname -m)
-
-echo "updating repos"
 sudo apt-get update
-echo "removing old ffmpeg and x264"
-sudo apt-get remove ffmpeg x264 libx264-dev
-echo "installing dependencies for ffmpeg and x264"
-sudo apt-get install build-essential checkinstall git cmake libfaac-dev libjack-jackd2-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libsdl1.2-dev libtheora-dev libva-dev libvdpau-dev libvorbis-dev libx11-dev libxfixes-dev libxvidcore-dev texi2html yasm zlib1g-dev
-echo "installing gstreamer and gtk"
-sudo apt-get install libgstreamer0.10-0 libgstreamer0.10-dev gstreamer0.10-tools gstreamer0.10-plugins-base libgstreamer-plugins-base0.10-dev gstreamer0.10-plugins-good gstreamer0.10-plugins-ugly gstreamer0.10-plugins-bad gstreamer0.10-ffmpeg libgtk2.0-0 libgtk2.0-dev libjpeg8 libjpeg8-dev axel
-echo "installing cmake"
-sudo apt-get install cmake cmake-curses-gui
-
-echo -n "Starting to download opencv from the git repository..."
-
-git clone git://github.com/Itseez/opencv.git
-cd opencv*
-git checkout -b 2.4.10
-
-echo -n "Do you want to build all examples ?"
-read text
-mkdir build
-cd build
-if [ "$text" = "y" ]; then
-	cmake -DCMAKE_BUILD_TYPE=Release -D BUILD_EXAMPLES=ON ..
-else
-	cmake -DCMAKE_BUILD_TYPE=Release ..
-fi
-echo "compiling opencv"
-make -j$cores
-sudo make install	
-cd ..
-echo "OpenCv successfully installed."
+sudo apt-get install build-essential libgtk2.0-dev libjpeg62-dev libtiff4-dev libjasper-dev libopenexr-dev cmake python-dev python-numpy libtbb-dev libeigen2-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev
+cd ~
+echo "Creating a directory for installation"
+mkdir ubuntu_installer
+cd ubuntu_installer
+echo "Downloading ffmpeg files"
+wget http://ffmpeg.org/releases/ffmpeg-0.7-rc1.tar.gz
+echo "Unpacking files...."
+tar -xvzf ffmpeg-0.7-rc1.tar.gz
+cd ffmpeg-0.7-rc1
+echo "Configuring unpacked files..."
+./configure --enable-gpl --enable-version3 --enable-nonfree --enable-postproc --enable-libfaac --enable-libopencore-amrnb --enable-libopencore-amrwb --enable-libtheora --enable-libvorbis --enable-libxvid --enable-x11grab --enable-swscale --enable-shared
+make
+echo "Installing files and dependancies"
+sudo make install
+cd ../
+wget -c http://garr.dl.sourceforge.net/project/opencvlibrary/opencv-unix/2.4.8/opencv-2.4.8.zip
+unzip opencv-2.4.8.zip
+cd opencv-2.4.8/
+cmake -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=OFF -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON .
+make
+echo "Installing openCV to your system, Please wait..."
+sudo make install
+echo "Installation done, configuring system"
+sudo touch /etc/ld.so.conf.d/opencv.conf 
+sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+echo "Reloading configuration files created."
+sudo ldconfig
+sudo sh -c 'echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig" >>/etc/bash.bashrc'
+sudo sh -c 'echo "export PKG_CONFIG_PATH" >>/etc/bash.bashrc'
+sudo cp /usr/local/lib/python2.7/site-packages/cv.so /usr/local/lib/python2.7/dist-packages/cv.so
+echo "Installation Done."
 
 # __END_MAIN__
