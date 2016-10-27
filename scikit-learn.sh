a5d0706fd7dc963b6732159777d9eeae76b4820a
Nikhil Kasukurthi <nikhil.kasukurthi@gmail.com>
2016-10-27 20:48:38 +0530

    Scikit-learn added

---
+++ scikit-learn.sh
@@ -0,0 +1,12 @@
+#Scikit-learn is used for Data Analysis and Machine Learning
+#They have many algorithms to use as libraries
+
+#Install PIP if not installed
+echo '************Installing pip************'
+sudo apt-get install python-pip -y
+
+echo '************Installing numpy************'
+sudo pip install -U numpy
+
+echo '************Installing Scikit-learn************'
+sudo pip install -U scikit-learn
